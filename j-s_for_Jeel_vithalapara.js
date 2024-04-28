ae09602f2723ba2e1266c47a5c43bf4666e772e4
Jeel0806 <104964531+Jeel0806@users.noreply.github.com>
2024-04-28 10:52:30 +0530

    Add files via upload

---
+++ j-s_for_Jeel_vithalapara.js
@@ -0,0 +1,52 @@
+const gallery = document.querySelector('.gallery');
+const track = document.querySelector('.gallery-track');
+const track1 = document.querySelector('.mainphoto');
+const cards = document.querySelectorAll('.card');
+const easing = 0.10;
+let startY = 0;
+let endY = 0;
+let raf;
+
+const lerp = (start,end,t) => start * (1-t) + end * t;
+
+function updateScroll() {
+  startY = lerp(startY,endY,easing);
+  gallery.style.height = `${track.clientHeight}px`;
+  track.style.transform = `translateY(-${startY}px)`;
+  activateParallax();
+  raf = requestAnimationFrame(updateScroll);
+  if (startY.toFixed(1) === window.scrollY.toFixed(1)) cancelAnimationFrame(raf);
+}
+
+function startScroll() {
+  endY = window.scrollY; 
+  cancelAnimationFrame(raf);
+  raf = requestAnimationFrame(updateScroll);
+}
+
+function parallax(card) {
+  const wrapper = card.querySelector('.card-image-wrapper');
+  const diff = card.offsetHeight - wrapper.offsetHeight;
+  const {top} = card.getBoundingClientRect();
+  const progress = top / window.innerHeight;
+  const yPos = diff * progress;
+  wrapper.style.transform = `translateY(${yPos}px)`;
+}
+
+const activateParallax = () => cards.forEach(parallax);
+
+function init() {
+  activateParallax();
+  startScroll();
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
+
+window.addEventListener('load',updateScroll,false);
+window.addEventListener('scroll',init,false);
+window.addEventListener('resize',updateScroll,false);
